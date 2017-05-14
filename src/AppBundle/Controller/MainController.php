dd299e4da53907955458c52eeaf7444a551896ab
Alkss <alkss_fjv@yahoo.com.br>
2017-05-14 19:00:15 -0300

    .

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -26,6 +27,7 @@ class MainController extends Controller
     }
     /**
      *@Route("/char-page")
+     * @Template()
      */
 
     public function showCharPage(){
@@ -36,7 +38,7 @@ class MainController extends Controller
 
     /**
      * @Route("char-page.html.php?name={name}&server={server}&region={region}" , name="char_page")
-     *
+     * @Template()
      */
     public function getCharPage($name, $server, $region){
 
