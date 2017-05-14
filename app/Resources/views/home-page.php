4fd680775a11076ecf0d75fa4ff9cadd5de796ca
Alkss <alkss_fjv@yahoo.com.br>
2017-05-14 19:03:36 -0300

    .

--- app/Resources/views/home-page.php
+++ app/Resources/views/home-page.php
@@ -1,14 +1,17 @@
+<?php
+$control = new \AppBundle\Controller\MainController();
+?>
+
 <html>
 <header>
 </header>
 <h1>WoWChecker</h1>
-    <form action="char-page.php">
+    <form action="<?php $control->getCharPage()?>">
     <h4> Buscar por Personagem </h4>
         Nome: <input type="text" name="name" /><br/>
         Server: <input type="text" name="server" /><br/>
         Região: <input type="text" name="region" /></br>
-		<input type="submit" value="Buscar">        
-   	    <a href="char-page.html.php" type="input">Buscar por char</a>
+		<input type="submit" value="Buscar">
     </form>
    	<hr>
     <form action="guild-page.php">
