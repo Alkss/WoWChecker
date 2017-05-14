db1f3af36cd2b42e405fa9c9201058430929836c
Alkss <alkss_fjv@yahoo.com.br>
2017-05-14 17:28:21 -0300

    algumas paginas

---
+++ src/AppBundle/Controller/CharPageController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 14/05/17
+ * Time: 02:05
+ */
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends Controller
+{
+
+    /**
+    *@Route("/")
+     */
+
+    public function showAction(){
+     //   @Route::class;
+        $templating = $this->container->get('templating');
+       return $this->render('novo-formulario.php');
+
+    }
+}
