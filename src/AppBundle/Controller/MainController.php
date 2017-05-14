30a77788a8181594411d27f797f6c124dd10c655
Alkss <alkss_fjv@yahoo.com.br>
2017-05-14 02:37:14 -0300

    first try

---
+++ src/AppBundle/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 14/05/17
+ * Time: 02:05
+ */
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController
+{
+
+    /**
+    *@Route("/index")
+     */
+
+    public function showAction(){
+     //   @Route::class;
+        return new Response('Olá!');
+
+    }
+}
